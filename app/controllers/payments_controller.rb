bae0aa61091d0223c493d3aa26790f76e960f4f3
shareena fatima <shareenafatima40@gmail.com>
2024-07-17 23:46:15 +0500

    add payment controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,5 +3,22 @@ class PaymentsController < ApplicationController
   end
 
   def create
+    @amount = 1
+
+    customer = Stripe::Customer.create(
+      email: params[:email],
+      source: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: 'CAMHS Consultation Session',
+      currency: 'gbp'
+    )
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to root_path
   end
 end
